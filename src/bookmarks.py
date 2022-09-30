5db060364c1d876668b2387d14b754ff085b57b9
Jennifer Gaupel <jennifergaupel@gmail.com>
2022-09-30 17:04:45 -0400

    Added user JWT authentication and refresh, create, retrieve bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,8 +1,78 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import validators
+from src.database import Bookmark, db
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/v1/bookmarks")
 
 
-@bookmarks.get('/')
-def get_all():
-    return {"bookmarks": []}
+@bookmarks.route('/', methods=['POST', 'GET'])
+@jwt_required()
+def handle_bookmarks():
+    current_user = get_jwt_identity()
+    if request.method == 'POST':
+        body = request.get_json().get('body', '')
+        url = request.get_json().get('url', '')
+
+        if not validators.url(url):
+            return jsonify({
+                'error': "Enter a valid URL"
+            }), HTTP_400_BAD_REQUEST
+
+        if Bookmark.query.filter_by(url=url).first():
+            return jsonify({
+                'error': "URL already exists"
+            }), HTTP_409_CONFLICT
+
+        bookmark = Bookmark(url=url, body=body,user_id=current_user)        
+        db.session.add(bookmark)
+        db.session.commit()
+
+        return jsonify({
+            'id': bookmark.id,
+            'url': bookmark.url,
+            'short_url': bookmark.short_url,
+            'visits': bookmark.visits,
+            'body': bookmark.body,
+            'created_at': bookmark.created_at,
+            'updated_at': bookmark.updated_at
+        }), HTTP_201_CREATED
+    
+    else:
+        bookmarks = Bookmark.query.filter_by(user_id=current_user)
+
+        data = []
+
+        for bookmark in bookmarks:
+            data.append({
+                'id': bookmark.id,
+                'url': bookmark.url,
+                'short_url': bookmark.short_url,
+                'visits': bookmark.visits,
+                'body': bookmark.body,
+                'created_at': bookmark.created_at,
+                'updated_at': bookmark.updated_at                
+            })
+
+        return jsonify({'data': data}), HTTP_200_OK
+
+@bookmarks.get('/<int:id>')
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id = current_user, id = id).first()
+
+    if not bookmark:
+        return jsonify({'message': "Item not found"}), HTTP_404_NOT_FOUND
+    
+    return jsonify({
+                'id': bookmark.id,
+                'url': bookmark.url,
+                'short_url': bookmark.short_url,
+                'visits': bookmark.visits,
+                'body': bookmark.body,
+                'created_at': bookmark.created_at,
+                'updated_at': bookmark.updated_at           
+            }), HTTP_200_OK
